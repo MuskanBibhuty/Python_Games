48c005b28bd90b3cf8efda6778d1a877779bb4cf
Muskan Bibhuty <muskanbibhuty@gmail.com>
2024-11-18 00:59:39 +0530

    Add files via upload

---
+++ Project _1_(Stone_Paper_Scissor).py
@@ -0,0 +1,45 @@
+'''
+1 for stone
+2 for paper
+3 for scissor
+'''
+
+import random
+
+def game(n = random.randint(1,3)):
+
+    if(n>3):
+        print("Something went wrong!")
+        exit()
+    
+    else:
+        computer = n
+        user = input("Enter your choice: ")
+        youstr = user.lower()
+        youdict = {"stone":1, "paper":2, "scissor":3}
+        you = youdict[youstr]
+        
+        reversedict = {1:"stone", 2:"paper", 3:"scissor"}
+        print("Computer choose", reversedict[computer])
+        print("You choose", reversedict[you])
+
+        if(computer==you):
+            print("It's a draw!")
+        
+        else:
+            if(computer==1 and you==2):
+                print("You Win!")
+            elif(computer==1 and you==3):
+                print("You Lose!")
+            elif(computer==2 and you==1):
+                print("You Lose!")
+            elif(computer==2 and you==3):
+                print("You Win!")
+            elif(computer==3 and you==1):
+                print("You Win!")
+            elif(computer==3 and you==2):
+                print("You Lose!")
+            else:
+                print("Something went wrong!")
+
+game()
